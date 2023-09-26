ce0acfea512f940a0b80bc5e60876f3f68647774
kaiiiverson <146119484+kaiiiverson@users.noreply.github.com>
2023-09-26 19:39:41 +0000

    here is a simple node js program to test stuff

--- server-test.js
+++ server-test.js
@@ -1,1 +1,13 @@
-hola;
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+// Define a route and send a response
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
