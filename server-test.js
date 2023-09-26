0d78836aed19f3088ffd1ed87670c9311cddfcd3
kaiiiverson <146119484+kaiiiverson@users.noreply.github.com>
2023-09-26 22:37:28 +0000

    attempted to add basic sql stuff. Enter user name, get back bio.

--- server-test.js
+++ server-test.js
@@ -1,13 +1,43 @@
-const express = require('express');
-const app = express();
-const PORT = 8080;
+const express = require('express')
+const morgan = require('morgan')
+const mysql = require('mysql2')
+
+const app = express()
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+})
+
+
+
+async function getUser(username) {
+  const [bio] = await pool.promise().query("SELECT bio FROM User WHERE username = ?", [
+    username,
+  ])
+  return bio[0]
+}
 
 // Define a route and send a response
 app.get('/', (req, res) => {
-  res.send('<h style="color:blue;">Fuck you garcia</h>');
-});
+  res.send('<h style="color:blue;">Hello world! Future home of bubble! (although bubble will be an android app and not a website)</h>')
+})
+
+app.get('/:username', async (req, res) => {
+  try {
+    const username = parseInt(req.params.username)
+    const bio = await getUser(username)
+    res.send(bio)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Listening on port ${port}`))
