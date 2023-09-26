7d097c39bb8baed1a0df5126bc05b4bbd7638a8b
kaiiiverson <146119484+kaiiiverson@users.noreply.github.com>
2023-09-26 20:24:29 +0000

    asdf

--- server-test.js
+++ server-test.js
@@ -4,7 +4,7 @@ const PORT = 8080;
 
 // Define a route and send a response
 app.get('/', (req, res) => {
-  res.send('<h style="color:blue;">Hello, world!</h>');
+  res.send('<h style="color:blue;">Fuck you garcia</h>');
 });
 
 // Start the server
