c93b10dad431b85435a969b8b123e7a0ba5b0c70
kaiiiverson <146119484+kaiiiverson@users.noreply.github.com>
2023-09-28 03:30:40 +0000

    refactored routes out of index

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ const morgan = require('morgan')
 const mysql = require('mysql2')
 const bodyParser = require('body-parser');
 
-const app = express()
+// Importing routes
+const accounts = require('./routes/accounts')
 
+// Setting up express
+const app = express()
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
 app.use(bodyParser.json())
 
+// Setting up mysql
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'MyNewPass1!',
   database: process.env.MYSQL_DATABASE || 'bubbleDB',
 })
+
+
 
 
 // Recieves username and returns that users bio
@@ -25,40 +31,16 @@ async function getUser(username) {
   return bio[0]
 }
 
+
+// Applying routes
+app.use('/:username', userRoutes);
+
+
 // Default response
 app.get('/', (req, res) => {
   res.send('<h style="color:blue;">Hello world! Future home of bubble! (although bubble will be an android app and not a website)</h>')
 })
 
-// Way to get bio by inputting username in URL
-app.get('/:username', async (req, res) => {
-  try {
-    const username = req.params.username
-    console.log(`${username} entered in for username`)
-    const bio = await getUser(username)
-    res.send(bio)
-  } catch (error) {
-    res.send(error)
-  }
-})
-
-// Define a PUT route to update a user's bio
-app.put('/:username', async (req, res) => {
-  try {
-    const username = req.params.username;
-    const newBio = req.body.bio; // Assuming the request contains a JSON body with a "bio" property
-
-    // Update the user's bio in the database
-    await pool.promise().query("UPDATE accounts SET bio = ? WHERE username = ?", [
-      newBio,
-      username,
-    ]);
-
-    res.send('Bio updated successfully')
-  } catch (error) {
-    res.status(500).send('Error updating bio: ' + error.message)
-  }
-})
 
 
 // Start the server
