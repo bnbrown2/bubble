1d6dae0aea336c90a89faaf717cbdb81716ac925
kaiiiverson <146119484+kaiiiverson@users.noreply.github.com>
2023-10-04 00:20:57 +0000

    server now returns test.html

--- index.js
+++ index.js
@@ -2,15 +2,14 @@
 const express = require('express')
 const morgan = require('morgan')
 const mysql = require('mysql2')
-const bodyParser = require('body-parser');
-
-// Importing routes
-const accounts = require('./routes/accounts')
+const bodyParser = require('body-parser')
+const path = require('path')
 
 // Setting up express
 const app = express()
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'sample_files')))
 
 // Setting up mysql
 const pool = mysql.createPool({
@@ -31,10 +30,6 @@ async function getUser(username) {
   return bio[0]
 }
 
-
-// Applying routes
-app.use('/account/:username', accounts);
-
 // Way to get bio by inputting username in URL
 app.get('/account/:username', async (req, res) => {
   try {
@@ -52,7 +47,7 @@ app.get('/account/:username', async (req, res) => {
 
 // Default response
 app.get('/', (req, res) => {
-  res.send('<h style="color:blue;">Hello world! Future home of bubble! (although bubble will be an android app and not a website)</h>')
+  res.sendFile(path.join(__dirname, 'sample_file', 'test.html'))
 })
 
 
