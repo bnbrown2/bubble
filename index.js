3d7525d519ae55b857795c9e8d4d8ee0dd60ce09
kaiiiverson <146119484+kaiiiverson@users.noreply.github.com>
2023-10-10 06:34:24 +0000

    Commented the index file better

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-// Import required modules
+/*
+*   Main file for the Bubble server
+*   Written in node.js
+*   Code created by Ben Brown and Kai Iverson
+*   
+*   Helpful tip: type `npm run devStart` into the command line to run server in dev mode
+*/
+
+
+// Import required modules.
 const express = require('express')
 const morgan = require('morgan')
 const mysql = require('mysql2')
 const bodyParser = require('body-parser')
 const path = require('path')
 
-// Setting up app
+// Setting up app. Also adding anything app needs access to.
 const app = express()
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'sample_files')))
 
-// Setting up mysql pool
+// Setting up mysql pool. You can set these values in the commend line or use the default values.
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   user: process.env.MYSQL_USER || 'root',
@@ -19,7 +28,7 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_DATABASE || 'bubbleDB',
 })
 
-// Import and use routes
+// Import and use routes. Routes are all in the routes folder.
 const accountRouter = require('./routes/account')
 const loginRouter = require('./routes/login')
 const registerRouter = require('./routes/register')
@@ -27,6 +36,6 @@ app.use('/account', accountRouter)
 app.use('/login', loginRouter)
 app.use('/register', registerRouter)
 
-// Start the server
+// Start the server. You can set the port in the command line or use the default value.
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Listening on port ${port}`))
