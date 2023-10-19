3ebb72840b261762648e53a8c781dace1225798b
kaiiiverson <146119484+kaiiiverson@users.noreply.github.com>
2023-10-19 23:42:53 +0000

    asdf

---
+++ routes/web/webAccount.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const axios = require('axios')
+const router = express.Router()
+
+router.route('/:username')
+    .get( async (req, res) => {
+
+        const username = req.params.username;
+    
+        const apiUrl = `http://54.202.77.126:8080/api/account/${username}`;
+        const response = await axios.get(apiUrl);
+        
+        // Extract the user info from the API response
+        const accJSON = response.data;
+
+        // Render the HTML page using a template engine (EJS in this case)
+        res.render('account', { accJSON });
+    })
