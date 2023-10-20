e59d5023eb059a51d9c7950e3e2824eeffe2f30c
kaiiiverson <146119484+kaiiiverson@users.noreply.github.com>
2023-10-20 00:24:37 +0000

    asdf

--- routes/web/webAccount.js
+++ routes/web/webAccount.js
@@ -7,17 +7,14 @@ router.route('/:username')
         const username = req.params.username
     
         const apiUrl = `http://54.202.77.126:8080/api/account/${username}`
-        const response = await axios.get(apiUrl)
-        
-        // Extract the user info from the API response
-        if (!response.data) {
-            res.json({error: 'account does not exist'})
-            return
+
+        try {
+            const response = await axios.get(apiUrl)
+            const accJSON = response.data
+            res.render('account', accJSON )
+        } catch(error) {
+            res.status(400).json({'error': error})
         }
-        const accJSON = response.data
-
-
-        res.render('account', accJSON )
     })
 
 module.exports = router
