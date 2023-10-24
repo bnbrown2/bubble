32d9f09ccce2c20ce2d46ce364dfbb4628d87b83
kaiiiverson <146119484+kaiiiverson@users.noreply.github.com>
2023-10-24 01:49:21 +0000

    asdf

--- routes/web/webSearch.js
+++ routes/web/webSearch.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const axios = require('axios')
 const router = express.Router()
 
 router.route('/')
-.get( (req, res) => {
-    res.render('search')
-})
+    .get( async (req, res) => {
+        const searchTerm = req.query.searchTerm
+
+        if (!searchTerm) {
+            return res.render('search')
+        }
+    
+        const apiUrl = `http://54.202.77.126:8080/api/search/}`
+
+        try {
+            const response = await axios.get(apiUrl)
+            const resJSON = response.data
+            res.render('search', resJSON )
+        } catch(error) {
+            res.status(400).json({'error': 'Account probably does not exist'})
+        }
+    })
 
 module.exports = router
