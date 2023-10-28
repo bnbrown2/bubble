a2082f2bcb6850c0f4a3763c44657bf474e0d4d1
kaiiiverson <146119484+kaiiiverson@users.noreply.github.com>
2023-10-28 03:02:42 +0000

    asdf

--- routes/api/image.js
+++ routes/api/image.js
@@ -8,15 +8,15 @@ const { uploadFile, getFileStream  } = require('../../s3')
 router
     .route('/profile_picture/u/:key')
     .get( async (req, res) => {
-        console.log("hello")
         const key = req.params.key
-        var readStream = getFileStream(`profile_picture/u/${key}`)
+        let readStream = getFileStream(`profile_picture/u/${key}`)
 
         readStream.on('error', (err) => {
             // Handle the error when the S3 object doesn't exist
             if (err.code === 'NoSuchKey') {
-              // Serve the default profile picture instead
-              readStream = getFileStream(`profile_picture/u/default`)
+                // Serve the default profile picture instead
+                const defaultReadStream = getFileStream(`profile_picture/u/default`)
+                defaultReadStream.pipe(res)
             } else {
               // Handle other errors
               res.status(500).end()
