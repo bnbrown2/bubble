ae71e118544cb836dec8a660c9c034cc3a860899
kaiiiverson <146119484+kaiiiverson@users.noreply.github.com>
2023-10-30 01:16:30 +0000

    asdf

--- routes/api/account.js
+++ routes/api/account.js
@@ -146,13 +146,13 @@ router
             }
 
             if (affectedRows > 0) {
-                res.status(200).json({ message: `Updated ${affectedRows} row`})
+                return res.status(200).json({ message: `Updated ${affectedRows} row`})
             } else {
-                res.status(404).json({ error: 'Updated 0 rows'})
+                return res.status(404).json({ error: 'Updated 0 rows'})
             }
         } catch(error) {
             console.error('Error updating account:', error)
-            res.status(500).json({ error: 'Internal server error'})
+            return res.status(500).json({ error: 'Internal server error'})
         }
     })
 
