80ae1a5e6cb731f35b4694dccde67ea23abd8326
kaiiiverson <146119484+kaiiiverson@users.noreply.github.com>
2023-11-15 01:04:53 +0000

    asdf

--- routes/api/image.js
+++ routes/api/image.js
@@ -26,4 +26,26 @@ router
         readStream.pipe(res)
     })
 
+router
+    .route('/posts/:username/:postID')
+    .get( async (req, res) => {
+        const username = req.params.username
+        const postID = req.params.postID
+        let readStream = getFileStream(`posts/${username}/${postID}`)
+
+        readStream.on('error', (err) => {
+            // Handle the error when the S3 object doesn't exist
+            if (true) {//err.code === 'NoSuchKey') {
+                // Serve the default profile picture instead
+                const defaultReadStream = getFileStream(`posts/bubble/defaultPost`)
+                defaultReadStream.pipe(res)
+            } else {
+              // Handle other errors
+              res.status(500).end()
+            }
+        })
+
+        readStream.pipe(res)
+    })
+
 module.exports = router
